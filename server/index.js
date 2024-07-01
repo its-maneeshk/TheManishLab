8e99de24c1e95d0c719e0a89acc6408f25558bd5
Manish Kumar Patel <maneeshkurmii@gmail.com>
2024-07-01 12:00:00 +0530

    MADE SOME CHANGES IN 'index.js' on 2024-07-01

--- server/index.js
+++ server/index.js
@@ -13,8 +13,6 @@ app.use(cors());
 PORT = process.env.PORT || 4040;
 
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
 })
 .then(() => console.log('MongoDB connected!'))
 .catch((err) => console.log(`DB Error: ${err.message}`));
